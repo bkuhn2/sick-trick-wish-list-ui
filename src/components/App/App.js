66c3d4db478d1e605709fe1df8df37ce8dab927c
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-02 14:00:05 -0700

    Created functionality via useEffect so that on page load, state is set to the preexisting tricks via useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import fetchData from '../../APIcalls';
 
 const App = () => {
 
   const [tricks, setTricks] = useState([]);
 
   const getTricks = () => {
-    
+    fetchData('http://localhost:3001/api/v1/tricks')
+      .then(data => {
+        // console.log('data: ', data);
+        setTricks(data);
+      })
   }
+
+  useEffect(() => {
+    getTricks();
+  }, [])
 
   return (
     <div className="App">
