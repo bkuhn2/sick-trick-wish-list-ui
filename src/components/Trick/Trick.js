83b8c0a6625036b2d4b94acff0951dcdc8228d2b
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-02 14:07:24 -0700

    Added basic imports, function and JSX for trick component

---
+++ src/components/Trick/Trick.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Trick.css';
+
+const Trick = ({stance, name, obstacle, tutorial, id}) => {
+
+  return (
+    <div className="trick">
+      <p>{stance} {name}</p>
+      <p>Obstacle: {obstacle}</p>
+      <p>Link to Tutorial:</p>
+      <a href={tutorial}>{tutorial}</a>
+    </div>
+  )
+}
