e9dea67ca373bf3e6b6277c903d98088963527c2
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-02 13:48:51 -0700

    Reconfigured App component to use hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sick Trick Wish List</h1>
-      </div>
-    );
+const App = () => {
+
+  const [tricks, setTricks] = useState([]);
+
+  const getTricks = () => {
+    
   }
+
+  return (
+    <div className="App">
+      <h1>Sick Trick Wish List</h1>
+    </div>
+  );
 }
 
 export default App;
